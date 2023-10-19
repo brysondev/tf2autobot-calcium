5d0b175deb9ca6da3ae9df1871fe3809198e8950
brysondev <bryson@brysondev.io>
2023-10-19 16:53:58 -0400

    flipped offered/asked to see in push notification

--- src/lib/tools/summarizeOffer.ts
+++ src/lib/tools/summarizeOffer.ts
@@ -67,8 +67,8 @@ export function summarizeToChat(
         `\n\n${cTSummary}${
             isOfferSent !== undefined ? ` (${isOfferSent ? 'chat' : `offer${isCountered ? ' - countered' : ''}`})` : ''
         }\n` +
-        `${cTAsked} ${generatedSummary.asked}` +
         `\n${cTOffered} ${generatedSummary.offered}` +
+        `${cTAsked} ${generatedSummary.asked}` +
         '\n──────────────────────' +
         (['summary-accepted', 'review-admin'].includes(type) && !isOfferSent
             ? value.diff > 0
